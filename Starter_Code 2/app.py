8f566c05d904da62b116dbc46725e9908c407035
samuelhfish <125224990+samuelhfish@users.noreply.github.com>
2023-05-01 11:15:33 -0400

    part 2 started

--- Starter_Code 2/app.py
+++ Starter_Code 2/app.py
@@ -1,30 +1,58 @@
 # Import the dependencies.
-
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurements = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return the precipitation data as json"""
+
+    return jsonify(Measurements)
+
+
+# @app.route("/")
+# def welcome():
+#     return (
+#         f"Welcome to the Sam's Surfs Up API!<br/>"
+#         f"Available Routes:<br/>"
+#         f"/api/v1.0/precipitation<br/>"
+#         f"/api/v1.0/stations<br/>"
+#         f"/api/v1.0/tobs<br/>"
+#         f"/api/v1.0/<start>"
+#         f"/api/v1.0/<start>/<end><br/>"
+#     )
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
